ca7a03cfcb5af882695b8f74d6d73c971ee06a6f
Shakthi Prasad G S <shakthi.gs87@gmail.com>
2018-06-30 13:35:15 +0530

    Added pam trees

--- src/universe_parts/hut.js
+++ src/universe_parts/hut.js
@@ -1,30 +1,68 @@
 
 defineThreeUniverse(function (THREE, options) {
 
-    return new Promise(function (resolve, reject) {
-
+    function loadMTLNObject(baseUrl, mtl, obj) {
         var objLoader = new THREE.OBJLoader2();
-        var callbackOnLoad = function (event) {
-            event.detail.loaderRootNode.scale.set(30, 30, 30);
 
-            event.detail.loaderRootNode.traverse(object => {
+        return new Promise((resolve, reject) => {
+            objLoader.loadMtl(baseUrl + mtl, null, function (materials) {
+
+                objLoader.setMaterials(materials);
+                objLoader.setLogging(false, false);
+                objLoader.load(baseUrl + obj, (event) => {
+                    resolve(event.detail.loaderRootNode);
+                }, null, null, null, false);
+
+            });
+        });
+
+    }
+
+    return new Promise(function (resolve, reject) {
+        var rootObj = new THREE.Object3D();
+        
+
+        var objectPromise2 = loadMTLNObject(options.baseUrl, 'resource/forest/Palm_Tree.mtl', 'resource/forest/Palm_Tree.obj');
+        objectPromise2.then(object => {
+
+            
+
+            object.scale.set(30, 30, 30);
+
+            object.traverse(object => {
                 if (object.isMesh) {
                     object.castShadow = true;
                 }
 
             })
-            event.detail.loaderRootNode.rotateY(25*Math.PI/180)
-            resolve(event.detail.loaderRootNode);
 
-        };
-        var onLoadMtl = function (materials) {
+            let prg = THREE.seedrandom("Palm Distribution with hut");
 
-            objLoader.setMaterials(materials);
-            objLoader.setLogging(false, false);
-            objLoader.load(options.baseUrl + 'resource/hut/shack.obj', callbackOnLoad, null, null, null, false);
+            for (let index = 0; index < 10; index++) {
+                
+                let clone=object.clone();
+                clone.rotateY(prg()*Math.PI*2);
+                clone.position.set(prg()*300-150+600,0,prg()*100-50);
+                rootObj.add(clone);
+            }
 
 
-        };
-        objLoader.loadMtl(options.baseUrl + 'resource/hut/shack.obj.mtl', null, onLoadMtl);
+            loadMTLNObject(options.baseUrl, 'resource/hut/shack.obj.mtl', 'resource/hut/shack.obj').then(hut=>{
+                rootObj.add(hut);
+                hut.scale.set(30, 30, 30);                
+                hut.position.set(300,0,200);
+                hut.rotateY(25 * Math.PI / 180)
+
+                
+                resolve(rootObj);
+            });
+
+        });
+
+
+
     });
 });
+
+
+
