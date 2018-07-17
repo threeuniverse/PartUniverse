a6c983176985fff5c6f570ffa626711fc01d0f51
Shakthi Prasad G S <shakthi.gs87@gmail.com>
2018-07-17 23:09:04 +0530

    Fixed loading of texture

--- src/universe_parts/futureGround.js
+++ src/universe_parts/futureGround.js
@@ -1,54 +1,60 @@
-defineThreeUniverse(function (THREE,options) {
+defineThreeUniverse(function (THREE, options) {
 
-    // floor
+    return new Promise(function (resolve) {
 
-    var vertex = new THREE.Vector3();
-    var color = new THREE.Color();
-    var floorGeometry = new THREE.PlaneBufferGeometry(2000, 2000, 20, 20);
-    floorGeometry.rotateX(- Math.PI / 2);
 
-    // vertex displacement
-    var position = floorGeometry.attributes.position;
 
-    for (var i = 0; i < position.count; i++) {
+        var vertex = new THREE.Vector3();
+        var color = new THREE.Color();
+        var floorGeometry = new THREE.PlaneBufferGeometry(2000, 2000, 20, 20);
+        floorGeometry.rotateX(- Math.PI / 2);
 
-        vertex.fromBufferAttribute(position, i);
+        // vertex displacement
+        var position = floorGeometry.attributes.position;
 
-        vertex.x += Math.random() * 20 - 10;
-        vertex.y += Math.random() * 10;
-        vertex.z += Math.random() * 20 - 10;
+        for (var i = 0; i < position.count; i++) {
 
-        position.setXYZ(i, vertex.x, vertex.y, vertex.z);
+            vertex.fromBufferAttribute(position, i);
 
-    }
+            vertex.x += Math.random() * 20 - 10;
+            vertex.y += Math.random() * 10;
+            vertex.z += Math.random() * 20 - 10;
 
-   // floorGeometry = floorGeometry.toNonIndexed(); // ensure each face has unique vertices
+            position.setXYZ(i, vertex.x, vertex.y, vertex.z);
 
-    let count = floorGeometry.attributes.position.count;
-    var colors = [];
+        }
 
-    for (var i = 0; i < count; i++) {
+        // floorGeometry = floorGeometry.toNonIndexed(); // ensure each face has unique vertices
 
-        color.setHSL(Math.random() * 0.5 + 0.5, 0.75, Math.random() * 0.1 + 0.75);
-        colors.push(color.r, color.g, color.b);
+        let count = floorGeometry.attributes.position.count;
+        var colors = [];
 
-    }
+        for (var i = 0; i < count; i++) {
 
-    var loader = new THREE.TextureLoader();
-    
+            color.setHSL(Math.random() * 0.5 + 0.5, 0.75, Math.random() * 0.1 + 0.75);
+            colors.push(color.r, color.g, color.b);
 
-    var normalMap = loader.load(options.baseUrl+'resource/Leather_004_NRM.png');
-    normalMap.wrapS = normalMap.wrapT = THREE.RepeatWrapping;
-    normalMap.repeat.set(25, 25);
-    
-    
+        }
 
-    
-    floorGeometry.addAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
+        var loader = new THREE.TextureLoader();
 
-    var floorMaterial = new THREE.MeshStandardMaterial({ metalness:0.5,normalMap:normalMap,flatShading:true,vertexColors: THREE.VertexColors,shininess:20 });
 
-    var floor = new THREE.Mesh(floorGeometry, floorMaterial);
-    return floor;
+        var normalMap = loader.load(options.baseUrl + 'resource/Leather_004_NRM.png', () => {
+            resolve(floor);
+        });
+        normalMap.wrapS = normalMap.wrapT = THREE.RepeatWrapping;
+        normalMap.repeat.set(25, 25);
+
+
+
+
+        floorGeometry.addAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
+
+        var floorMaterial = new THREE.MeshStandardMaterial({ metalness: 0.5, normalMap: normalMap, flatShading: true, vertexColors: THREE.VertexColors, shininess: 20 });
+
+        var floor = new THREE.Mesh(floorGeometry, floorMaterial);
+        
+    })
+
 
 });
