16a3b1868b154de54d380981a4298e558c250032
Shakthi Prasad G S <shakthi.gs87@gmail.com>
2018-09-13 23:14:54 +0530

    Simplified hut

--- src/universe_parts/hut.js
+++ src/universe_parts/hut.js
@@ -1,13 +1,12 @@
 
-defineThreeUniverse(function (THREE, UNIVERSE, options) {
+defineThreeUniverse(function (THREE, UNIVERSE, SPACE) {
 
   
 
     return new Promise(function (resolve, reject) {
         var rootObj = new THREE.Object3D();
 
-        var objectPromise2 = UNIVERSE.loadMTLNObject(options.baseUrl, 'resource/forest/Palm_Tree.mtl', 'resource/forest/Palm_Tree.obj');
-        objectPromise2.then(object => {
+        SPACE.loadMTLNObject('resource/forest/Palm_Tree.obj').then(object => {
 
 
 
@@ -23,7 +22,7 @@ defineThreeUniverse(function (THREE, UNIVERSE, options) {
                 clone.rotateY(prg() * Math.PI * 2);
                 clone.position.set(prg() * 300 - 150 + 600, 100, prg() * 100 - 50);
 
-                options.GetGroundHitPoint(clone.position).then((result) => {
+                SPACE.GetGroundHitPoint(clone.position).then((result) => {
 
                     clone.position.y = result[0].point.y;
 
@@ -32,14 +31,14 @@ defineThreeUniverse(function (THREE, UNIVERSE, options) {
                 rootObj.add(clone);
             }
 
-            options.loadMTLNObject( 'resource/hut/shack.obj').then(hut => {
+            SPACE.loadMTLNObject( 'resource/hut/shack.obj').then(hut => {
                 rootObj.add(hut);
                 hut.scale.set(30, 30, 30);
                 hut.position.set(300, 0, 200);
                 hut.rotateY(25 * Math.PI / 180)
 
 
-                options.GetGroundHitPoint(new THREE.Vector3()).then((result) => {
+                SPACE.GetGroundHitPoint(new THREE.Vector3()).then((result) => {
 
                     hut.position.y = result[0].point.y - 5;
 
