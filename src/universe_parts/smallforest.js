e660573572511bd00f6e2c85f2388251e3380e95
Shakthi Prasad G S <shakthi.gs87@gmail.com>
2018-09-19 23:54:11 +0530

    Fixed small forest

--- src/universe_parts/smallforest.js
+++ src/universe_parts/smallforest.js
@@ -1,54 +1,30 @@
 
-defineThreeUniverse(function (THREE,UNIVERSE,options) {
+defineThreeUniverse(function (THREE, UNIVERSE, SPACE) {
 
     return new Promise(function (resolve, reject) {
 
-        var objLoader = new THREE.OBJLoader2();
-        var callbackOnLoad = function (event) {
-            
-            // //console.log('Loading complete: ' + event.detail.modelName);
-            // //scope._reportProgress({ detail: { text: '' } });
-            // event.detail.loaderRootNode.rotateX(- 90 * THREE.Math.DEG2RAD);
-
-            let prg = UNIVERSE.seedrandom("Farest rendering start");
+        SPACE.loadMTLNObject('resource/forest/Oak_Tree.obj').then((rootnode) => {
             let obj = new THREE.Object3D();
-            event.detail.loaderRootNode.scale.set(40, 40, 40);
-            
-            event.detail.loaderRootNode.traverse(object => {
-                if (object.isMesh) {
-                    object.castShadow = true;
-                }
 
-            })
+            rootnode.scale.set(40, 40, 40);
+            UNIVERSE.castShadow(rootnode);
+            let prg = UNIVERSE.seedrandom("Farest rendering start");
 
             for (let index = 0; index < 20; index++) {
-                
-                let clone=event.detail.loaderRootNode.clone();
-                clone.rotateY(prg()*Math.PI*2);
-                clone.position.set(prg()*1000-500,100,prg()*1000-500);
-                options.GetGroundHitPoint(clone.position).then(result=>{
-                    clone.position.y=result[0].point.y;
+
+                let clone = rootnode.clone();
+                clone.rotateY(prg() * Math.PI * 2);
+                clone.position.set(prg() * 1000 - 500, 100, prg() * 1000 - 500);
+                SPACE.GetGroundHitPoint(clone.position).then(result => {
+                    clone.position.y = result[0].point.y;
                     obj.add(clone);
                 });
 
-              
-                
             }
-
-
-
             resolve(obj);
 
-        };
-        var onLoadMtl = function (materials) {
-            
-            // objLoader.setModelName(modelName);
-            objLoader.setMaterials(materials);
-            objLoader.setLogging(true, true);
-            objLoader.load(options.baseUrl+'resource/forest/Oak_Tree.obj', callbackOnLoad, null, null, null, false);
+        })
 
 
-        };
-        objLoader.loadMtl(options.baseUrl+'resource/forest/Oak_Tree.mtl', null, onLoadMtl);
     });
 });
