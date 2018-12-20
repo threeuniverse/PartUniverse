88d0f570523327c9c7ca6b81a3ebcc9f2f718953
Shakthi Prasad G S <shakthi.gs87@gmail.com>
2018-12-20 22:48:46 +0530

    Updated maping to use organisation repo

--- src/universe_parts/mapping.js
+++ src/universe_parts/mapping.js
@@ -15,12 +15,12 @@ defineThreeUniverse(function () {
         {
             position: { x: 0, z: 0 },
             credits: "https://www.turbosquid.com/",
-            url: "https://shakthi.github.io/PartUniverse/src/universe_parts/lonetree.js",
+            url: "https://threeuniverse.github.io/PartUniverse/src/universe_parts/lonetree.js",
         },
         {
             position: { x: 100, z: 100 },
             credits: "https://www.turbosquid.com/",
-            url: "https://shakthi.github.io/PartUniverse/src/universe_parts/smallforest.js",
+            url: "https://threeuniverse.github.io/PartUniverse/src/universe_parts/smallforest.js",
         },
         {
             position: { x: 0, z: 0 },
@@ -32,18 +32,18 @@ defineThreeUniverse(function () {
             position: { x: 0, z: -2700, y: 0 },
             description: "Futeristic ground",
             radius: 1500,
-            url: "https://shakthi.github.io/PartUniverse/src/universe_parts/futureGround.js",
+            url: "https://threeuniverse.github.io/PartUniverse/src/universe_parts/futureGround.js",
         },
          {
             position: { x: 200, z: -2500 },
             description: "IcosahedronGeometry",
-            url: "https://shakthi.github.io/PartUniverse/src/universe_parts/sampleMeshModule.js"
+            url: "https://threeuniverse.github.io/PartUniverse/src/universe_parts/sampleMeshModule.js"
 
         },
         {
             position: { x: -1000, z: -2900 },
             description: "IcosahedronGeometry",
-            url: "https://shakthi.github.io/PartUniverse/src/universe_parts/sampleMeshModule.js"
+            url: "https://threeuniverse.github.io/PartUniverse/src/universe_parts/sampleMeshModule.js"
         },
         {
             position: { x: 542, y: 0, z: -1900 },
@@ -52,7 +52,7 @@ defineThreeUniverse(function () {
         }, {
             position: { x: 600, z: 100 },
             credits: "https://www.turbosquid.com/",
-            url: "https://shakthi.github.io/PartUniverse/src/universe_parts/hut.js",
+            url: "https://threeuniverse.github.io/PartUniverse/src/universe_parts/hut.js",
         },
 
 
